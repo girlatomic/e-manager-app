b7251b4c1e0dcf384c6ff41bddde9af011cda6e3
Gaby Pineda <gabygabypineda@gmail.com>
2022-02-23 09:21:35 +0100

    adds clients and repairs tables with initial data

--- model/init_db.sql
+++ model/init_db.sql
@@ -1,28 +1,30 @@
+DROP TABLE IF EXISTS repairs;
+
 CREATE TABLE repairs (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    model VARCHAR(100),
-    brand VARCHAR(100),
-    client_id INT NOT NULL,
-    FOREIGN KEY (client_id) REFERENCES clients(id)
-);
-​
-CREATE TABLE clients (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    first_name VARCHAR(100),
-    last_name VARCHAR(100),
-    phone VARCHAR(100)
+    model VARCHAR(100) NOT NULL,
+    brand VARCHAR(100) NOT NULL
 );
 
-INSERT INTO repairs (model, brand) 
+INSERT INTO repairs (model, brand)
 VALUES
-    ('iPhone X', 'Apple'),  
-    ('Galaxy S22', 'Samsung'); 
-​
-INSERT INTO clients (first_name, last_name, phone) 
-VALUES 
-    ('Sue', 'Andrews', 689678527),
-    ('Luis', 'Alvarez', 31608187),
-    ('Sam', 'Huston', 23849950),
-    ('Maria', 'Sorgaard', 1239587),
-    ('Ahmed', 'Omar', 19399273),
-    ('Angelica', 'Busconi', 2399938);
+    ('iphone X', 'Apple'),
+    ('Galaxy S22', 'Samsung');
+
+
+DROP TABLE IF EXISTS clients;
+
+CREATE TABLE clients (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL
+);
+
+INSERT INTO clients (first_name, last_name)
+VALUES
+    ('Sue', 'Andrews'),
+    ('Alex', 'Sasov'),
+    ('Alejandra', 'Ortiz'),
+    ('Thom', 'Yorke'),
+    ('Rafael', 'Pineda'),
+    ('Luis', 'Alvarez');
